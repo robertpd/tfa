5c5395e5bc9acc94a798ac95bb002cd2654edb5d
rob <robertofdooley@gmail.com>
2020-04-26 18:42:38 -0400

    will it commit?

--- resources.tf
+++ resources.tf
@@ -14,6 +14,6 @@ resource "aws_instance" "database" {
 }
 
 resource "aws_instance" "fmt" {
-  ami = "ami-085925f297f89fce1"
+  ami           = "ami-085925f297f89fce1"
   instance_type = "t3a.nano"
 }
