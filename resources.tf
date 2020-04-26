99564ec75a29024c1d74caf7801f3c716f7e2cba
rob <robertofdooley@gmail.com>
2020-04-26 18:48:47 -0400

    pre commit test

--- resources.tf
+++ resources.tf
@@ -17,3 +17,8 @@ resource "aws_instance" "fmt" {
   ami           = "ami-085925f297f89fce1"
   instance_type = "t3a.nano"
 }
+
+resource "aws_instance" "pre-commit-test" {
+  instance_type = "t3a.nano"
+  ami= "ami-085925f297f89fce1"
+}
