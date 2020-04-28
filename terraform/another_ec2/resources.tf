bde43c2cf77031089e08738d5e746d9a0a3254b3
rob <robertofdooley@gmail.com>
2020-04-28 18:24:51 -0400

    tf change

--- terraform/another_ec2/resources.tf
+++ terraform/another_ec2/resources.tf
@@ -10,5 +10,5 @@ resource "aws_instance" "another_example" {
 
 resource "aws_instance" "changed" {
   ami           = "ami-085925f297f89fce1"
-  instance_type = "t3a.nano"
+  instance_type = "t3a.medium"
 }
