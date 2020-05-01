14baa91176b84e20e2b514543a60f45091b0d8e9
rob <robertofdooley@gmail.com>
2020-04-30 20:01:19 -0400

    test fmt

--- terraform/another_ec2/resources.tf
+++ terraform/another_ec2/resources.tf
@@ -14,6 +14,6 @@ resource "aws_instance" "changed" {
 }
 
 resource "aws_instance" "pull_request" {
-  ami= "ami-085925f297f89fce1"
+  ami = "ami-085925f297f89fce1"
   instance_type = "t3a.medium"
 }
